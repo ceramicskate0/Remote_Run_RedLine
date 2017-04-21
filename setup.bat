29619e7ea05056aaff20aa04dcef8e30e08073f7
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-04-20 21:55:21 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 mkdir C:\Tools\
 mkdir C:\Tools\RedLineAnalysis
+mkdir C:\Tools\RedLineAnalysis\Analysis
 if NOT exist C:\Tools\RedLineAnalysis\EmailEvent.ps1 echo MISSING EMAIL EVENT SCRIPT
 echo Please use Redline to create all the droppers you wish to use and house them in the Dir, C:\Tools\RedLineAnalysis
